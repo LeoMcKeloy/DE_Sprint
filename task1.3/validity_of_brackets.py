b13769fcc920dad7c293a58e61dc52fe11a9b17d
LPyryanov <lpyryanov@beeline.ru>
2022-10-09 21:59:08 +0300

    Commit task 1.3

---
+++ task1.3/validity_of_brackets.py
@@ -0,0 +1,21 @@
+def brackets_validate(string):
+    symbol_array = [x for x in string]
+    brackets = []
+    for symbol in symbol_array:
+        if symbol == "{" or symbol == "[" or symbol == "(":
+            brackets.append(symbol)
+        elif symbol == "}":
+            if brackets.pop() == "{":
+                continue
+            return False
+        elif symbol == "]":
+            if brackets.pop() == "[":
+                continue
+            return False
+        elif symbol == ")":
+            if brackets.pop() == "(":
+                continue
+            return False
+    if len(brackets) == 0:
+        return True
+    return False
